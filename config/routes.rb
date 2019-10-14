898a001e351cb89ace0a6943ca138e68b2754360
Luke Prendergast <luke.prendergast94@gmail.com>
2019-10-14 22:23:04 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres, only: [:show,:edit,:create,:update, :new]
+  resources :artists, only: [:index,:show,:edit,:create,:update, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
